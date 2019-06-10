ada7e16a0af95e31a1960969f05082bed09ef2d7
ChrisTrevisan <48164188+ChrisTrevisan@users.noreply.github.com>
2019-06-10 02:33:57 -0400

    Update RecordsList.java

--- src/RecordsList.java
+++ src/RecordsList.java
@@ -29,7 +29,7 @@ public class RecordsList {
     public static ArrayList<Record> readRecords() {
         ArrayList<Record> arr = new ArrayList<>(0);
         try {
-            Scanner in = new Scanner(new File("Interconnected-Isolation/assets/files/records.txt"));
+            Scanner in = new Scanner(new File(System.getProperty("user.home") + "/Desktop/records.txt"));
             while (in.hasNextLine()) {
                 String line = in.nextLine();
                 arr.add(new Record(line.substring(0, line.indexOf(' ')), Integer.parseInt(line.substring(line.indexOf(' ') + 1))));
@@ -39,6 +39,8 @@ public class RecordsList {
         return arr;
     }
 
+
+
     /**
      * Clears all the records from the file.
      *
@@ -46,7 +48,7 @@ public class RecordsList {
      */
     public static void clearRecords() {
         try {
-            PrintWriter out = new PrintWriter(new FileWriter("Interconnected-Isolation/assets/files/records.txt"));
+            PrintWriter out = new PrintWriter(new FileWriter(System.getProperty("user.home") + "/Desktop/records.txt"));
             out.close();
         } catch (IOException e) {
         }
