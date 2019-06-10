bc988f8776ac4aa7b3106142c63f0091ab4b045e
ChrisTrevisan <48164188+ChrisTrevisan@users.noreply.github.com>
2019-06-10 02:33:18 -0400

    Update PlayerRecords.java

--- src/PlayerRecords.java
+++ src/PlayerRecords.java
@@ -7,6 +7,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * Creates a scene which displays all of the player records.
@@ -27,8 +28,29 @@ public class PlayerRecords implements Screen {
     /**
      * Buttons which the user can click.
      */
-    private Button backToMenu, clear;
-
+    private Button backToMenu, clear, name, percent;
+    private boolean increasing;
+    private ArrayList<Record> arr;
+    private Text[] names,percents;
+    private BorderPane layout1;
+    private GridPane layout2;
+    private StackPane layout3;
+    private HBox layout4;
+    private Text text1,text2,text3;
+    private boolean nameLast, percentLast;
+    private static void mergesort(ArrayList<Record> arr,int l, int r, Comparator<Record> comp) {
+        if (l >= r) return;
+        int mid = (l+r)>>1;
+        mergesort(arr,l,mid,comp);
+        mergesort(arr,mid+1,r,comp);
+        ArrayList<Record> sorted = new ArrayList<>();
+        int firstl = l, firstr = mid+1;
+        while (firstl <= mid || firstr <= r) {
+            if (firstr > r || (firstl <= mid && comp.compare(arr.get(firstl),arr.get(firstr)) < 0)) sorted.add(arr.get(firstl++));
+            else sorted.add(arr.get(firstr++));
+        }
+        for (int x = l; x <= r; x++) arr.set(x,sorted.get(x-l));
+    }
     /**
      * Creates and formats the scene for the player records scene.
      * Adds all buttons and text.
@@ -36,15 +58,7 @@ public class PlayerRecords implements Screen {
      * @return the player records scene
      * @since 1.11
      */
-    public Scene window() {
-        BorderPane layout1 = new BorderPane();
-        GridPane layout2 = new GridPane();
-        StackPane layout3 = new StackPane();
-        HBox layout4 = new HBox(50);
-        ArrayList<Record> arr = RecordsList.readRecords();
-        Text[] names = new Text[arr.size()];
-        Text[] percents = new Text[arr.size()];
-
+    private void reWrite () {
         for (int i = 0; i < arr.size(); i++) {
             names[i] = new Text();
             percents[i] = new Text();
@@ -55,42 +69,66 @@ public class PlayerRecords implements Screen {
             percents[i].setStyle("-fx-font-family: 'Quicksand', sans-serif; -fx-font-size: 20px");
             percents[i].setFill(Color.WHITE);
         }
-
-        Text text1 = new Text("Player Records");
-        text1.setStyle("-fx-font-family: 'Megrim', cursive; -fx-font-size: 60px");
-        text1.setFill(Color.WHITE);
-        layout3.getChildren().add(text1);
-        layout3.setPadding(new Insets(30, 10, 30, 10));
-
-        Text text2 = new Text("Name:");
-        text2.setStyle("-fx-font-family: 'Megrim', cursive; -fx-font-size: 40px");
-        text2.setFill(Color.WHITE);
+        layout2 = new GridPane();
         layout2.add(text2, 0, 0);
-
-        Text text3 = new Text("Percent Improved:");
-        text3.setStyle("-fx-font-family: 'Megrim', cursive; -fx-font-size: 40px");
-        text3.setFill(Color.WHITE);
         layout2.add(text3, 1, 0);
-
         for (int i = 0; i < arr.size(); i++) {
             layout2.add(names[i], 0, i + 1);
             layout2.add(percents[i], 1, i + 1);
         }
         layout2.setHgap(80);
         layout2.setVgap(10);
+        layout2.setAlignment(Pos.TOP_CENTER);
+        layout1.setCenter(layout2);
+        percent.setMinWidth(300);
+        name.setMinWidth(300);
+        percent.setText(percentLast ? (increasing ? "Sort by Percent v" : "Sort by Percent ^") : "Sort by Percent");
+        name.setText(nameLast ? (increasing ? "Sort by Name v" : "Sort by Name ^") : "Sort by Name");
+    }
+    public Scene window() {
+        nameLast = percentLast = false;
+        layout1 = new BorderPane();
+        layout2 = new GridPane();
+        layout3 = new StackPane();
+        layout4 = new HBox(50);
+        arr = RecordsList.readRecords();
+        names = new Text[arr.size()];
+        percents = new Text[arr.size()];
+        increasing = true;
+
+        text1 = new Text("Player Records");
+        text1.setStyle("-fx-font-family: 'Megrim', cursive; -fx-font-size: 60px");
+        text1.setFill(Color.WHITE);
+        layout3.getChildren().add(text1);
+        layout3.setPadding(new Insets(30, 10, 30, 10));
+
+        text2 = new Text("Name:");
+        text2.setStyle("-fx-font-family: 'Megrim', cursive; -fx-font-size: 40px");
+        text2.setFill(Color.WHITE);
+
+        text3 = new Text("Percent Improved:");
+        text3.setStyle("-fx-font-family: 'Megrim', cursive; -fx-font-size: 40px");
+        text3.setFill(Color.WHITE);
 
         clear = new Button("Clear Records");
         clear.setStyle("-fx-background-color: #3a3a3a; -fx-font-family: 'Megrim', cursive; -fx-font-size: 30px");
         clear.setTextFill(Color.WHITE);
 
 
+        name = new Button("Sort by Name ^");
+        name.setStyle("-fx-background-color: #3a3a3a; -fx-font-family: 'Megrim', cursive; -fx-font-size: 30px");
+        name.setTextFill(Color.WHITE);
+
+        percent = new Button("Sort by Percent ^");
+        percent.setStyle("-fx-background-color: #3a3a3a; -fx-font-family: 'Megrim', cursive; -fx-font-size: 30px");
+        percent.setTextFill(Color.WHITE);
+
         backToMenu = new Button("Main Menu");
         backToMenu.setStyle("-fx-background-color: #3a3a3a; -fx-font-family: 'Megrim', cursive; -fx-font-size: 30px");
         backToMenu.setTextFill(Color.WHITE);
-
-        layout4.getChildren().addAll(clear, backToMenu);
+        reWrite();
+        layout4.getChildren().addAll(clear, backToMenu,name,percent);
         layout4.setPadding(new Insets(30, 10, 30, 10));
-
         layout3.setAlignment(Pos.CENTER);
         layout2.setAlignment(Pos.TOP_CENTER);
         layout4.setAlignment(Pos.CENTER);
@@ -106,7 +144,28 @@ public class PlayerRecords implements Screen {
             InterconnectedIsolation.clearRecords();
             ;
         });
-
+        name.setOnAction(e -> {
+            mergesort(arr, 0, arr.size() - 1, new Comparator<Record>() {
+                @Override
+                public int compare(Record o1, Record o2) {
+                    return increasing ? o1.getName().compareTo(o2.getName()) : o2.getName().compareTo(o1.getName());
+                }
+            });
+            increasing = !increasing;
+            nameLast = true; percentLast = false;
+            reWrite();
+        });
+        percent.setOnAction(e -> {
+            mergesort(arr, 0, arr.size() - 1, new Comparator<Record>() {
+                @Override
+                public int compare(Record o1, Record o2) {
+                    return increasing ? Integer.compare(o1.getPercent(),o2.getPercent()) : Integer.compare(o2.getPercent(),o1.getPercent());
+                }
+            });
+            increasing = !increasing;
+            percentLast = true; nameLast = false;
+            reWrite();
+        });
         return new Scene(layout1, 1280, 720);
     }
 
